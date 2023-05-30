fa4d933f3319a7de3bef84e6ab997da60ea69432
LG <LG@192.168.200.156>
2023-05-30 15:59:29 +0900

    Add leap_max_days, update calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,48 +5,49 @@ import java.util.Scanner;
 public class Calendar {
 
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
 	
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month -1];
-	}
-
-	public void printSampleCalendar() {
-		System.out.println("  일 월 화 수 목 금 토 ");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-
-	}
-	
-	
-	public static void main(String[] args) {
-		// 숫자를 입력받아서 해당하는 달의 최대일수를 출력하기
-		
-		
-		String PROMPT = "cal> ";
-		Scanner sc = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		
-		int month = 0;
-		while (true) {
-			System.out.println("해당 월을 입력하세요: ");
-			System.out.print(PROMPT);
-			month = sc.nextInt();
-			if(month == -1) {
-				break;
-			}
-			
-			if(month > 12) {
-				continue;
-			}
-			System.out.printf("%d월은 %d까지 있습니다.\n" , month, cal.getMaxDaysOfMonth(month));
-			
+	public boolean isLeapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 != 0)) {
+			return true;
+		} else {
+			return false;
 		}
-		System.out.println("::end::");
-		sc.close();
-		
-		
 	}
+	
+	public int getMaxDaysOfMonth(int year, int month) {
+		if(isLeapYear(year)) {
+			return  LEAP_MAX_DAYS[month -1];
+		} else {
+			return MAX_DAYS[month -1];
+		}
+	}
+
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%4d %3d>>\n", year, month);
+		System.out.println(" SU MO TH WE TH FR SA");
+		System.out.println("----------------------");
+		
+				
+		int maxDay = getMaxDaysOfMonth(year, month);
+		
+		for(int i = 1; i<= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if(i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		
+		System.out.println();
+		
+		
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+
+	}
+	
+	
+
 }
